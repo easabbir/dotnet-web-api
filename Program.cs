09bdf6f23cf72b25cd13198e7ec70fc7e4399f73
Ehsanul Alam Sabbir <easabbir@gmail.com>
2024-10-07 00:04:29 +0600

    more api methods added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+
 app.UseHttpsRedirection();
+
+
+app.MapGet("/", () =>
+{
+    return "API is working fine";
+});
+
+app.MapGet("/hello", () => 
+{
+    return "Hello World! this is my first api";
+});
+
+app.MapPost("/hello",()=>
+{
+    return "Post method: Hello";
+});
+
+app.MapPut("/hello",()=>
+{
+    return "Put method: Hello";
+});
+
+app.MapPatch("/hello",()=>
+{
+    return "Put method: Hello";
+});
+
+app.MapDelete("/hello",()=>
+{
+    return "Delete method: Hello";
+});
+
+
+
 app.Run();
