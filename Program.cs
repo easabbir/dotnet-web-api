dbad4a6811e6207f6d3f80b692ba9ec99e47bdf9
Ehsanul Alam Sabbir <easabbir@gmail.com>
2024-10-10 20:17:53 +0600

    Basic crud achieved

--- Program.cs
+++ Program.cs
@@ -16,6 +16,52 @@ app.UseHttpsRedirection();
 app.MapGet("/", () => "API is working fine"
 );
 
+List<Category> categories = new List<Category>();
+
+//Get:  /api/categories => read a category
+app.MapGet("/api/categories",() => {
+    return Results.Ok(categories    );
+});
+
+
+//Post:  /api/categories => create a category
+app.MapPost("/api/categories",() => {
+    var newCategory=new Category
+    {
+        CategoryId = Guid .Parse("d1a653f0-5d2e-48fc-8a31-1b92dccc6b66"),
+        Name="Smartphone",
+        Description="This is the description of the smartphone",
+        CreatedAt=DateTime.UtcNow
+    };
+    categories.Add(newCategory);
+    return Results.Created($"/api/categories/{newCategory.CategoryId}", newCategory );
+});
+
+//Delete:  /api/categories => delete a category
+app.MapDelete("/api/categories",() => {
+    var foundCategory = categories.FirstOrDefault(category => category.CategoryId ==Guid .Parse("d1a653f0-5d2e-48fc-8a31-1b92dccc6b66"));
+    if(foundCategory==null){
+        return Results.NotFound("Category with this id does not exists");
+    }
+    categories.Remove(foundCategory);
+    return Results.NoContent();
+});
+
+
+//Update:  /api/categories => update a category
+app.MapPut("/api/categories",() => {
+    var foundCategory = categories.FirstOrDefault(category => category.CategoryId ==Guid .Parse("d1a653f0-5d2e-48fc-8a31-1b92dccc6b66"));
+    if(foundCategory==null){
+        return Results.NotFound("Category with this id does not exists");
+    }
+    foundCategory.Name="smartphone";
+    foundCategory.Description="updated description";
+    return Results.NoContent();
+});
+
+
+
+
 app.MapGet("/hello", () => 
 {
     
@@ -63,3 +109,17 @@ public record Product
 (
  String Name, Decimal Price
 );
+
+public record Category
+{
+    public Guid CategoryId { get; set; }
+    public String? Name { get; set; }
+    public String? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+};
+
+//CRUD 
+//Create => create a category => POST : /api/categories 
+//Read   => read a category   => GET : /api/categories
+//Update => update a category => PUT : /api/categories
+//Delete => delete a category => DELETE : /api/categories
